e214de81e44c3dfdf17f7050e90ecb1d4e1e932c
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-03-02 22:40:54 -0300

    chore: subindo usecase dos post

---
+++ src/use-case/services/post/get-post.ts
@@ -0,0 +1,32 @@
+import type { Post } from "@/@types/prisma/client.js"
+import type { PostsRepository } from "@/repositories/post-repository.js"
+
+
+interface getPostUseCaseRequest {
+    publicId: string // publicId do Usuário que irá fazer o post
+}
+
+type getPostUseCaseResponse = {
+    post: Post 
+}
+
+
+
+export class getPostUseCase {
+    constructor ( private PostsRepository: PostsRepository) {}
+
+    async execute({ // uma função, quando dentro de uma classe nos chamamos de método. O nome 'execute' é uma convensão 
+        publicId,
+    }: getPostUseCaseRequest): Promise<getPostUseCaseResponse> { 
+
+
+        const post = await this.PostsRepository.findBy({ publicId });
+
+        if (!post) {
+            throw new Error("post não encontrado")
+        }
+        
+        return { post }  
+    }
+
+}
