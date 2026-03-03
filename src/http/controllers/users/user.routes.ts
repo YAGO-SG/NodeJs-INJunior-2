e0f1cb627f536da8c80306ab9f8628c2789620de
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-03-02 23:40:22 -0300

    feat: implementando autenticação apra usuários

--- src/http/controllers/users/user.routes.ts
+++ src/http/controllers/users/user.routes.ts
@@ -5,12 +5,13 @@ import { deleteUser } from "./delete-user.controller.js";
 import { patchUser } from "./patch-user.controller.js";
 import { listOneUser } from "./list-one-user.controller.js";
 import { authenticate } from "./authenticate.controller.js";
+import { verifyJwt } from "@/http/middlewares/verify-jwt.js";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post('/', register);
     app.post('/authenticate', authenticate);
     app.get('/list', listUsers);
-    app.delete('/delete/:publicId', deleteUser);
-    app.patch('/update/:publicId', patchUser);
+    app.delete('/delete/:publicId', { onRequest: [verifyJwt]}, deleteUser);
+    app.patch('/update/:publicId', { onRequest: [verifyJwt]}, patchUser);
     app.get('/user/:publicId', listOneUser);
 }
