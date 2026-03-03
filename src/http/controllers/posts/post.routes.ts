ce7db234914853c301557d8f4dfd8d7c18b5df98
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-03-02 23:39:02 -0300

    implementando autenticação para a rota de post

--- src/http/controllers/posts/post.routes.ts
+++ src/http/controllers/posts/post.routes.ts
@@ -5,12 +5,13 @@ import { userPosts } from "./user-posts.controller.js";
 import { deletePost } from "./delete-post.controller.js";
 import { patchPost } from "./patch-post.controller.js";
 import { listOnePost } from "./list-one-post.controller.js";
+import { verifyJwt } from "@/http/middlewares/verify-jwt.js";
 
 export async function postRoutes(app: FastifyInstance) {
-    app.post('/:publicId', createPost);
+    app.post('/:publicId', { onRequest: [verifyJwt]}, createPost);
     app.get('/list', listPosts);
     app.get('/listUserPost/:publicId', userPosts)
-    app.delete('/delete/:publicId', deletePost);
-    app.patch('/update/:publicId', patchPost)
+    app.delete('/delete/:publicId', { onRequest: [verifyJwt]}, deletePost);
+    app.patch('/update/:publicId', { onRequest: [verifyJwt]}, patchPost)
     app.get('/list/:publicId', listOnePost)
 }
