cb18ee7dbe468b43a137c3309254789fc0b7a285
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-03-02 23:36:13 -0300

    feat: implementando autenticação em algumas rotas

--- src/http/controllers/coment/coment.routes.ts
+++ src/http/controllers/coment/coment.routes.ts
@@ -5,12 +5,14 @@ import { listComent } from "./list-coments.controller.js";
 import { getComent } from "./get-coment.controller.js";
 import { deleteComent } from "./delete-coment.controller.js";
 import { UpdateComent } from "./update-coment.controller.js";
+import { userComents } from "./user-coments.controller.js";
 
 
 export async function comentRoutes(app: FastifyInstance) {
     app.post('/:publicId', { onRequest: [verifyJwt]}, createComent);
     app.get('/', listComent)
     app.get('/:publicId', getComent)
-    app.delete('/delete/:publicId', deleteComent)
-    app.patch('/update/:publicId', UpdateComent)
+    app.delete('/delete/:publicId', { onRequest: [verifyJwt]}, deleteComent)
+    app.patch('/update/:publicId', { onRequest: [verifyJwt]}, UpdateComent)
+    app.get('/user/:publicId', userComents)
 }
